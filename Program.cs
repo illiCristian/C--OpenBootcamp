c37f7a9cdf0816b1e7e29ad08faec6d3502245bc
illiCristian <svrk73@gmail.com>
2023-01-17 22:20:48 -0300

    Ejercicio 1 y 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Ejercicio 1
 Console.WriteLine("Escribe tu nombre");
 string nombre = Console.ReadLine();
-
 Console.WriteLine("Tu nombre es " + nombre);
+// Ejercicio 2
+DateTime currentTime = DateTime.Now;
+Console.WriteLine(currentTime.ToString("h:mm tt"));
+
